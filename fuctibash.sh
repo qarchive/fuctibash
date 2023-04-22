e08c268e6e7aeb1534d4f1cbb6d6d8fe768cc676
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-04-22 21:57:32 +0300

    Update fuctibash.sh

--- fuctibash.sh
+++ fuctibash.sh
@@ -1,29 +1,9 @@
+#!/bin/bash
 # Fuctibash
-# echo "Please choose one of the following."
-# echo "1. Install a tty web browser."
-# echo "2. Update and upgrade system."
-# echo "3. Install cmatrix"
-# echo "4. Install fastfetch (Like neofetch, but much faster because written in C.)"
-# echo "5. Open DuckDuckGo"
 
-# Method 1
-
-#read secim
-
-#case $secim in
-#  1) sudo pacman -S links;;
-#  2) sudo pacman -Syu;;
-#  3) sudo pacman -S cmatrix;;
-#  4) git clone https://github.com/LinusDierheimer/fastfetch.git && cd fastfetch && mkdir -p build && cd build && cmake .. && cmake --build . --target fastfetch --target flashfetch;;
-#  5) exo-open https://duckduckgo.com;;
-#  *) echo "Geçersiz seçim";;
-# esac
-
-# Method 2
-
-select secim in "Install a tty web browser." "Update and upgrade system." "Install cmatrix" "Install fastfetch" "Open DuckDuckGo"
+select sel in "Install a tty web browser." "Update and upgrade system." "Install cmatrix" "Install fastfetch" "Open DuckDuckGo"
 do
-  case $secim in
+  case $sel in
     "Install a tty web browser.") sudo pacman -S links && exit;;
     "Update and upgrade system.") sudo pacman -Syu && exit;;
     "Install cmatrix") sudo pacman -S cmatrix && exit;;
