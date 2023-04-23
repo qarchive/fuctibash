7791cf4328603a5c53eac075b295fa57d6353705
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-04-23 12:07:05 +0300

    Update fuctibash.sh

--- fuctibash.sh
+++ fuctibash.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Fuctibash
+echo
+echo If you make a wrong choice, simply do the CTRL + c combination. The script will give you one more choice.
+echo
 
-select sel in "Install a tty web browser." "Update and upgrade system." "Install cmatrix" "Install fastfetch" "Open DuckDuckGo" "Install flatpak"
+select sel in "Install a tty web browser." "Update and upgrade system." "Install cmatrix" "Install fastfetch" "Open DuckDuckGo" "Install flatpak" "Install i3wm"
 do
   case $sel in
     "Install a tty web browser.") sudo pacman -S links && exit;;
@@ -10,6 +13,7 @@ do
     "Install fastfetch") git clone https://github.com/LinusDierheimer/fastfetch.git && cd fastfetch && mkdir -p build && cd build && cmake .. && cmake --build . --target fastfetch --target flashfetch && exit;;
     "Open DuckDuckGo") exo-open https://duckduckgo.com && exit;;
     "Install flatpak") sudo pacman -S flatpak && exit;;
+    "Install i3wm") sudo pacman -S i3;;
     *) echo "Geçersiz seçim && exit";;
   esac
 done
