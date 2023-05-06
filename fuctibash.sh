fb0b31206dadd335c37bfde1303d66980f301b34
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-05-06 18:45:33 +0300

    Update fuctibash.sh

--- fuctibash.sh
+++ fuctibash.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
-# Fuctibash
-echo
-echo If you make a wrong choice, simply do the CTRL + c combination. The script will give you one more choice.
-echo
 
-select sel in "Install a tty web browser." "Update and upgrade system." "Install cmatrix" "Install fastfetch" "Open DuckDuckGo" "Install flatpak" "Install i3wm"
-do
-  case $sel in
-    "Install a tty web browser.") sudo pacman -S links && exit;;
-    "Update and upgrade system.") sudo pacman -Syu && exit;;
-    "Install cmatrix") sudo pacman -S cmatrix && exit;;
-    "Install fastfetch") git clone https://github.com/LinusDierheimer/fastfetch.git && cd fastfetch && mkdir -p build && cd build && cmake .. && cmake --build . --target fastfetch --target flashfetch && exit;;
-    "Open DuckDuckGo") exo-open https://duckduckgo.com && exit;;
-    "Install flatpak") sudo pacman -S flatpak && exit;;
-    "Install i3wm") sudo pacman -S i3;;
-    *) echo "Geçersiz seçim && exit";;
-  esac
-done
+echo If you get an error, install dialog package.
+
+selection=$(dialog --menu "Lütfen seçiminizi yapın:" 10 60 3 1 "Install a tty web browser." 2 "Update and upgrade system." 3 "Install cmatrix" 4 "Install fastfetch" 5 "Open DuckDuckGo" 6 "Install flatpak" 7 "Install i3wm" --output-fd 1)
+
+case $selection in
+    1)
+        sudo pacman -S links --noconfirm && clear
+        ;;
+    2)
+        sudo pacman -Syu --noconfirm && clear
+        ;;
+    3)
+        sudo pacman -S cmatrix --noconfirm && clear
+        ;;
+    4)
+        git clone https://github.com/LinusDierheimer/fastfetch.git && cd fastfetch && mkdir -p build && cd build && cmake .. && cmake --build . --target fastfetch --target flashfetch && clear
+        ;;
+    5)
+	exo-open https://duckduckgo.com && clear
+	;;
+    6)
+	sudo pacman -S flatpak --noconfirm && clear
+	;;
+    7)
+	sudo pacman -S i3 --noconfirm && clear
+	;;
+
+esac
